2fbc2d0d4ac7632647cb3a812a7372307a56df1c
kkaonashi <43109053+qaeqwg@users.noreply.github.com>
2025-09-09 13:50:42 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-import streamlit as st
-from dotenv import load_dotenv, set_key, find_dotenv
-from graph import build_graph
-from tools import scrape_web_content
-
-load_dotenv()
-
-print(scrape_web_content("https://blog.naver.com/samsung_fn/223889175031"))
